088caa35af4d5666c185620884081273c7e550f6
Pascal <pascal.senizergues@gmail.com>
2020-11-04 10:23:42 +0100

    clean etape 3

--- bowling.py
+++ bowling.py
@@ -42,38 +42,6 @@ class Scorer():
         
         return sum(self._frame_score)
 
-    # def _calculate_shoot(self, shoot: int):
-    #     self._score += shoot
-    #     if self._is_first_shoot:
-    #         self._frame_score = shoot
-    #         if shoot == 10:
-    #             for prev_strike in self.strikes:
-    #                 prev_strike.append(shoot)
-    #                 if len(prev_strike) == 3:
-
-    #             self.strikes.append([10])
-
-    #             self._prev_strike = 2
-    #             self._is_first_shoot = False
-    #         elif self._prev_spare:
-    #             self._score += shoot
-    #             self._prev_spare = False
-    #         elif self._prev_strike > 0:
-    #             self._add_strike_bonus(shoot)
-
-    #     else:
-    #         self._frame_score += shoot
-    #         if self._frame_score == 10:
-    #             self._prev_spare = True
-    #         elif self._prev_strike > 0:
-    #             self._add_strike_bonus(shoot)
-
-    #     self._is_first_shoot = not self._is_first_shoot
-
-    # def _add_strike_bonus(self, shoot: int):
-    #     self._score += shoot
-    #     self._prev_strike = self._prev_strike - 1
-
 # class Partie():
 #     def __init__(self):
 #         pass
