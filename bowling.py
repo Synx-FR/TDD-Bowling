f90694d73e8c9f80421fa4dcb4d217e5a71165f1
Pascal <pascal.senizergues@gmail.com>
2020-11-04 10:22:52 +0100

    etape 3 complete

--- bowling.py
+++ bowling.py
@@ -1,49 +1,78 @@
+STRIKE = 10
 
 class Scorer():
     def __init__(self):
         self._is_first_shoot = True
         self._prev_spare = False
-        self._prev_strike = 0
-        self._score = 0
-        self._frame_score = 0
+        self._frame_score = []
 
     def calculate_total_score(self, shoots_list: []) -> int:
-        self._score = 0
-        self._frame_score = 0
+        self._frame_score = []
         self._is_first_shoot = True
-        self._prev_spare = False
-        self._prev_strike = 0
+        current_frame_score = 0
 
-        for shoot in shoots_list:
-            self._calculate_shoot(shoot)
+        idx = 0
+        tab_len = len(shoots_list)
+        while idx < tab_len:
+            if self._is_first_shoot:
+                current_frame_score = shoots_list[idx]
+
+                if current_frame_score == STRIKE:                  
+                    if (idx + 1) < tab_len:
+                        current_frame_score += shoots_list[idx + 1]
+
+                    if (idx + 2) < tab_len:
+                        current_frame_score += shoots_list[idx + 2]
+
+                    self._is_first_shoot = False
+                    self._frame_score.append(current_frame_score)
+                
+                elif idx == tab_len - 1:
+                        self._frame_score.append(current_frame_score)
+
+            else:
+                current_frame_score += shoots_list[idx]
+
+                if current_frame_score == STRIKE and (idx + 1) < tab_len:
+                    current_frame_score += shoots_list[idx + 1]
+                self._frame_score.append(current_frame_score)
+
+            self._is_first_shoot = not self._is_first_shoot
+            idx += 1
         
-        return self._score
+        return sum(self._frame_score)
 
-    def _calculate_shoot(self, shoot: int):
-        self._score += shoot
-        if self._is_first_shoot:
-            self._frame_score = shoot
-            if shoot == 10:
-                self._prev_strike = 2
-                self._is_first_shoot = False
-            elif self._prev_spare:
-                self._score += shoot
-                self._prev_spare = False
-            elif self._prev_strike > 0:
-                self._add_strike_bonus(shoot)
+    # def _calculate_shoot(self, shoot: int):
+    #     self._score += shoot
+    #     if self._is_first_shoot:
+    #         self._frame_score = shoot
+    #         if shoot == 10:
+    #             for prev_strike in self.strikes:
+    #                 prev_strike.append(shoot)
+    #                 if len(prev_strike) == 3:
 
-        else:
-            self._frame_score += shoot
-            if self._frame_score == 10:
-                self._prev_spare = True
-            elif self._prev_strike > 0:
-                self._add_strike_bonus(shoot)
+    #             self.strikes.append([10])
 
-        self._is_first_shoot = not self._is_first_shoot
+    #             self._prev_strike = 2
+    #             self._is_first_shoot = False
+    #         elif self._prev_spare:
+    #             self._score += shoot
+    #             self._prev_spare = False
+    #         elif self._prev_strike > 0:
+    #             self._add_strike_bonus(shoot)
 
-    def _add_strike_bonus(self, shoot: int):
-        self._score += shoot
-        self._prev_strike = self._prev_strike - 1
+    #     else:
+    #         self._frame_score += shoot
+    #         if self._frame_score == 10:
+    #             self._prev_spare = True
+    #         elif self._prev_strike > 0:
+    #             self._add_strike_bonus(shoot)
+
+    #     self._is_first_shoot = not self._is_first_shoot
+
+    # def _add_strike_bonus(self, shoot: int):
+    #     self._score += shoot
+    #     self._prev_strike = self._prev_strike - 1
 
 # class Partie():
 #     def __init__(self):
